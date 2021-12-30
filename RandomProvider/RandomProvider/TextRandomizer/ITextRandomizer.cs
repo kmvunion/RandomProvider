d4a01fafdfc03fbeb2b9e8642ef7c4097df26fdc
Nick <kanick@i.ua>
2021-12-30 22:57:04 +0200

    Simplify code

--- RandomProvider/RandomProvider/TextRandomizer/ITextRandomizer.cs
+++ RandomProvider/RandomProvider/TextRandomizer/ITextRandomizer.cs
@@ -21,14 +21,22 @@ namespace KMVUnion.RandomProvider.TextRandomizer
         /// Generate random noisy text
         /// </summary>
         /// <param name="sumbols">number of the symbols</param>
-        /// <returns>Enumerator of strings with noisy text</returns>
+        /// <returns>Enumerator of strings with generated string</returns>
         IEnumerable<string> GetNoisyText(int symbols);
 
         /// <summary>
         /// Generate random wordy text
         /// </summary>
         /// <param name="words">number of the words</param>
-        /// <returns>Enumerator of strings with wordy text</returns>
+        /// <returns>Enumerator of strings with generated string</returns>
         IEnumerable<string> GetWordyText(int words);
+
+        /// <summary>
+        /// Generate random and similar to the literature text.
+        /// It contains sentences and, each of those starts from a capital letter.
+        /// </summary>
+        /// <param name="words">number of words</param>
+        /// <returns>Enumerator of strings with generated string</returns>
+        IEnumerable<string> GetSentencefiedText(int words);
     }
 }
--- RandomProvider/RandomProvider/TextRandomizer/SymbolCases.cs
+++ RandomProvider/RandomProvider/TextRandomizer/SymbolCases.cs
@@ -1,4 +1,4 @@
-﻿namespace KMVUnion.RandomProvider.StringRandomizer
+﻿namespace KMVUnion.RandomProvider.TextRandomizer
 {
     public enum SymbolCases
     {
@@ -20,6 +20,6 @@
         /// <summary>
         /// Upper option provides all symbols in the upper case. 
         /// </summary>
-        Upper 
+        Upper
     }
 }
